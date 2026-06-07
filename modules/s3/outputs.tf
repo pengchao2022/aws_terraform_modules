9b0891ff9709d340e764e6cd49cfbae6d1e2fe2e
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-07 19:23:23 +0800

    feat: GTP-54273 modified the cdoe

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -8,6 +8,10 @@ output "bucket_id" {
   description = "Name of the S3 bucket"  # for s3 bucket, the bucket_id is also the name
 }
 
+output "bucket_name" {
+  value = aws_s3_bucket.main.bucket
+}
+
 output "website_url"{
   description = "The URL of the static website"
   value = var.enable_website ? "http://${aws_s3_bucket.main.id}.s3-website-us-east-1.amazonaws.com" : "Website not enabled"
