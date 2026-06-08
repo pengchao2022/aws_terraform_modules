7667e20fbb90cd3370027080a0d821326f815b63
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-08 18:43:48 +0800

    feat: GTP-54273 modified the code

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -8,3 +8,9 @@ output "lambda_name" {
   description = "The name of the Lambda function"
   value       = aws_lambda_function.this.function_name 
 }
+
+output "lambda_invoke_arn" {
+  description = "THe Invoke ARN of the lambda function"
+  value       = aws_lambda_function.this.invoke_arn
+  
+}
